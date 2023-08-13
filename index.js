39ef7531cd59ea5a5df16f969d7127484e947033
DeadlyAlien <nicog99@live.com>
2023-08-12 23:29:24 -0500

    working on form validation with JS

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 
+
+// submit function for email validation
+function validateEmail() {
+    let x = document.forms["emailForm"]["email"].value;
+    if (x.includes("@") && x.includes(".com")) {
+        alert("Success!")
+    } else {
+
+        document.querySelector(".inputemail").style.borderColor = "red"
+        document.querySelector(".inputemail").style.backgroundColor = "rgb(255, 236, 240)"
+        
+        return false
+
+    };
+};
+
