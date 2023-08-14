5a2c54f94aa8356c8fc0d7d5025f5a6f6ebbae3a
DeadlyAlien <nicog99@live.com>
2023-08-14 00:27:58 -0500

    Finished error state for form

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function validateEmail() {
     if (x.includes("@") && x.includes(".com")) {
         alert("Success!")
     } else {
-
         document.querySelector(".inputemail").style.borderColor = "red"
+        document.querySelector(".inputemail").style.color = "red"
         document.querySelector(".inputemail").style.backgroundColor = "rgb(255, 236, 240)"
-        
+        document.querySelector(".label2").style.visibility = "visible"
         return false
 
     };
