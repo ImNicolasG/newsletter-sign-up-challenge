ca68c587b5342a3546053d4db3265551d15e2b07
DeadlyAlien <nicog99@live.com>
2023-08-14 10:31:56 -0500

    tired af, trying to figure out saving input value and reapplying to next page

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 
 
 
+
+
 // submit function for email validation
 function validateEmail() {
-    let x = document.forms["emailForm"]["email"].value;
+    const x = document.forms["emailForm"]["email"].value;
+    
     if (x.includes("@") && x.includes(".com")) {
         
         window.open("success.html");
-        return true
+        return x;
     } else {
         document.querySelector(".inputemail").style.borderColor = "red"
         document.querySelector(".inputemail").style.color = "red"
         document.querySelector(".inputemail").style.backgroundColor = "rgb(255, 236, 240)"
         document.querySelector(".label2").style.visibility = "visible"
-        return false
+        return false;
 
     };
+    
 };
-
